6c77e806fea1cfac9f7005b90b84426efc32769f
kanghaeven <meerhae@gmail.com>
2025-01-17 12:21:14 +0900

    🔥Remove: 이전 posts 함수 및 컴포넌트 삭제

--- hlog/src/mdx-components.tsx
+++ hlog/src/mdx-components.tsx
@@ -8,12 +8,6 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     Link,
     BlogTitle,
     PostContent,
-    h1: (props) => <h1 className="text-3xl font-bold mt-8 mb-4" {...props} />,
-    h2: (props) => (
-      <h2 className="text-2xl font-semibold mt-6 mb-3" {...props} />
-    ),
-    p: (props) => <p className="mb-4" {...props} />,
-    // 다른 요소들에 대한 스타일 정의
     ...components,
   };
 }
