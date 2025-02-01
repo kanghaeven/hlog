fb12084c7adf71dda6c151f68cf4ad6ddec9e446
kanghaeven <meerhae@gmail.com>
2025-02-01 17:45:56 +0900

    💄Design: Post List, Title UI 변경

--- hlog/src/components/PostList.tsx
+++ hlog/src/components/PostList.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 interface Post {
   url: string;
@@ -6,6 +7,7 @@ interface Post {
   publishDate: string;
   categories: string[];
   posterImage?: string;
+  description?: string;
 }
 
 interface PostListProps {
@@ -14,24 +16,55 @@ interface PostListProps {
 
 const PostList = ({ posts }: PostListProps) => {
   return (
-    <ul className="p-0 list-none">
+    <ul className="w-full max-w-6xl p-0 m-0 mt-10 list-none">
       {posts.map((post) => (
         <li
           key={post.url}
-          className="p-0 m-0 border-b-[0.1rem] border-secondary"
+          className="p-0 m-0 border-t border-gray-200 first:border-t-0"
         >
           <Link
             href={post.url}
-            className="grid grid-cols-[1fr_4fr_1fr] items-center p-6 no-underline"
+            className="grid grid-cols-[39%_2%_59%] p-5 no-underline hover:bg-gray-50"
           >
-            <div className="text-sm font-light text-muted">
-              {post.categories.join(", ")}
+            {/* 왼쪽 이미지 영역 */}
+            <div className="flex items-stretch gap-12 jusity-between">
+              <div className="relative w-full aspect-[3/2]">
+                {post.posterImage && (
+                  <Image
+                    src={post.posterImage}
+                    alt={post.title}
+                    fill
+                    className="object-cover m-0"
+                  />
+                )}
+              </div>
+              <div className="flex flex-col items-end text-lg text-muted-foreground">
+                <span>
+                  {post.publishDate.split(" ")[0].split("-").slice(1).join(".")}
+                </span>
+                <span>{post.publishDate.split("-")[0]}</span>
+              </div>
             </div>
-            <div className="text-base leading-none text-foreground">
-              {post.title}
-            </div>
-            <div className="text-sm text-right font-extralight text-muted">
-              {post.publishDate.split(" ")[0]}
+            <div className=""></div>
+
+            {/* 오른쪽 텍스트 영역 */}
+            <div className="flex flex-col justify-between">
+              <div className="flex justify-between">
+                <h2 className="m-0 font-medium text-xxl">{post.title}</h2>
+                <span className="h-fit px-2 border-muted-foreground text-muted text-md rounded-full border-[1px] text-bold">
+                  {post.categories[0]}
+                </span>
+              </div>
+              <div className="flex items-end justify-between ">
+                <span className="max-w-xs text-xs text-muted-foreground">
+                  {post.description}
+                </span>
+                <div className="flex gap-2 m-0 text-sm text-muted">
+                  {post.categories.slice(1).map((category, index) => (
+                    <span key={index}>{category}</span>
+                  ))}
+                </div>
+              </div>
             </div>
           </Link>
         </li>
--- hlog/src/components/PostTitle.tsx
+++ hlog/src/components/PostTitle.tsx
@@ -3,16 +3,29 @@ import { Frontmatter } from "@/types/types";
 
 const PostTitle = ({
   title,
+  description,
   publishDate,
   posterImage,
   categories,
 }: Frontmatter) => {
   return (
-    <header>
-      <h1>{title}</h1>
-      <p>{new Date(publishDate).toLocaleDateString()}</p>
-      <p>{categories.join(", ")}</p>
-      <Image src={posterImage} alt="title" width={800} height={400} />
+    <header className="flex flex-col items-center justify-center">
+      <p className="text-base font-semibold text-primary">{categories[0]}</p>
+      <h1 className="font-bold leading-tight text-black">{title}</h1>
+      <p className="flex gap-3 m-0 text-base font-normal text-secondary">
+        {categories.slice(1).map((category, index) => (
+          <span key={index}>#{category}</span>
+        ))}
+      </p>
+      <p className="text-sm font-light text-muted-foreground">
+        {new Date(publishDate).toLocaleDateString("ko-KR", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </p>
+      <Image src={posterImage} alt="thumbnail" width={600} height={400} />
+      <p className="m-0 text-base text-muted font-regular">{description}</p>
     </header>
   );
 };
