1a93a4a1c2b4b1f07fe2852a676bc0cd11c6bebd
kanghaeven <meerhae@gmail.com>
2025-02-07 21:10:16 +0900

    💄Design: 코드 블럭 테마 색상

--- hlog/src/lib/mdxUtils.ts
+++ hlog/src/lib/mdxUtils.ts
@@ -2,10 +2,17 @@ import { compileMDX } from "next-mdx-remote/rsc";
 import { Frontmatter } from "@/types/types";
 import { getFileContent } from "@/lib/fileUtils";
 import remarkGfm from "remark-gfm";
-// import remarkToc from "remark-toc";
 import remarkA11yEmoji from "@fec/remark-a11y-emoji";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
+
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  theme: {
+    dark: "night-owl",
+    light: "catppuccin-latte",
+  },
+};
 
 export async function parseMdxFile(filePath: string) {
   const fileContent = await getFileContent(filePath);
@@ -15,20 +22,8 @@ export async function parseMdxFile(filePath: string) {
     options: {
       parseFrontmatter: true,
       mdxOptions: {
-        remarkPlugins: [
-          remarkGfm,
-          remarkA11yEmoji,
-          // [
-          //   remarkToc,
-          //   {
-          //     heading: "Table of Contents",
-          //     ordered: false,
-          //     tight: true,
-          //     maxDepth: 3,
-          //   },
-          // ],
-        ],
-        rehypePlugins: [[rehypePrettyCode, { theme: "night-owl" }], rehypeSlug],
+        remarkPlugins: [remarkGfm, remarkA11yEmoji],
+        rehypePlugins: [[rehypePrettyCode, options], rehypeSlug],
       },
     },
   });
