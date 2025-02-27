c35d9d26b6c7e024d70ff0ecf8095fc8a8eb73d7
kanghaeven <meerhae@gmail.com>
2025-02-27 16:38:37 +0900

    💄Design: 포스트 타이틀 제목 텍스트 크기 수정

--- hlog/src/components/PostTitle.tsx
+++ hlog/src/components/PostTitle.tsx
@@ -11,7 +11,9 @@ const PostTitle = ({
   return (
     <header className="flex flex-col items-center justify-center">
       <p className="text-base font-semibold text-primary">{categories[0]}</p>
-      <h1 className="mt-0 font-bold leading-tight text-black">{title}</h1>
+      <h1 className="mt-0 text-2xl font-bold leading-tight text-black sm:text-3xl">
+        {title}
+      </h1>
       <p className="flex gap-3 m-0 text-base font-normal text-secondary">
         {categories.slice(1).map((category, index) => (
           <span key={index}>#{category}</span>
