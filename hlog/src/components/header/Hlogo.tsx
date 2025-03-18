14e0d6c62010418187fd7e92281ad29404cd8a52
kanghaeven <meerhae@gmail.com>
2025-03-18 23:11:47 +0900

    🚀Feat: h 로고 클릭시 홈으로, 푸터 팝업 위치 조정

--- hlog/src/components/header/Hlogo.tsx
+++ hlog/src/components/header/Hlogo.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const Hlogo = () => {
   return (
-    <div className="px-2 text-4xl leading-none sm:px-1 sm:text-6xl text-primary">
-      H
-    </div>
+    <Link href="/" passHref className="no-underline">
+      <div className="px-2 text-4xl leading-none cursor-pointer sm:px-1 sm:text-6xl text-primary">
+        H
+      </div>
+    </Link>
   );
 };
 
