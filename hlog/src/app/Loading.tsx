c6228fb704c62028cf61cf2d4b2f89553b3d64cd
kanghaeven <meerhae@gmail.com>
2025-03-19 00:29:08 +0900

    💄Design: 게시글 스켈레톤 ui

--- hlog/src/app/Loading.tsx
+++ hlog/src/app/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center p-6 mx-auto mt-6 sm:mt-10">
+    <div className="flex justify-center w-full p-6 mx-auto mt-6 sm:mt-10">
       <div className="hidden max-w-xs mr-8 xl:block"></div>
 
       <div className="w-full max-w-3xl">
@@ -11,15 +11,6 @@ export default function Loading() {
           <div className="w-1/3 h-6 mb-6 bg-gray-200 rounded"></div>{" "}
           <div className="w-full h-56 mb-6 bg-gray-200 rounded"></div>{" "}
           <div className="w-5/6 h-8 mb-6 bg-gray-200 rounded"></div>{" "}
-        </div>
-
-        <div className="my-10 xl:hidden">
-          <div className="w-1/2 h-8 mb-6 bg-gray-200 rounded"></div>{" "}
-          <div className="space-y-4">
-            {[...Array(4)].map((_, index) => (
-              <div key={index} className="w-3/4 h-6 bg-gray-200 rounded"></div> // ToC 항목들
-            ))}
-          </div>
         </div>
 
         <div className="space-y-6">
