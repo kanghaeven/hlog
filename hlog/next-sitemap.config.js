725d140d38fcf2e356bc561ed5fe6b48707e83bd
kanghaeven <meerhae@gmail.com>
2025-04-16 00:03:50 +0900

    🚀Feat: sitemap에 카테고리 및 포스트 경로 자동 추가

--- hlog/next-sitemap.config.js
+++ hlog/next-sitemap.config.js
@@ -14,4 +14,27 @@ module.exports = {
       },
     ],
   },
+  additionalPaths: async () => {
+    const urls = [];
+
+    const categories = fs.readdirSync(POSTS_PATH);
+
+    for (const category of categories) {
+      const categoryPath = path.join(POSTS_PATH, category);
+      const posts = fs
+        .readdirSync(categoryPath)
+        .filter((file) => file.endsWith(".mdx"));
+
+      for (const post of posts) {
+        const postSlug = post.replace(/\.mdx$/, "");
+        urls.push({
+          loc: `/${category}/${postSlug}`,
+          changefreq: "daily",
+          priority: 0.7,
+        });
+      }
+    }
+
+    return urls;
+  },
 };
